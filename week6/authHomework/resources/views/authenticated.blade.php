2f776bcd48a73ba1e5923f74601ef3229d90390c
davidbenia <david.benia.1@btu.edu.ge>
2021-12-27 10:36:10 +0400

    Auth Breeze Homework

---
+++ week6/authHomework/resources/views/authenticated.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        body {
+            background-color: whitesmoke;
+        }
+
+        .main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+            width: 100%;
+        }
+
+        .main h1 {
+            color: gray
+        }
+
+        .main a {
+            text-decoration: none;
+            font-size: 30px;
+            font-weight: bold;
+            color: blue;
+        }
+
+    </style>
+    <title>Secret Page</title>
+</head>
+
+<body>
+    <div class="main">
+        <h1>Welcome To Secret Page</h1>
+        <a href="/">Go Back</a>
+    </div>
+</body>
+
+</html>
